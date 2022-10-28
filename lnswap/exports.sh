301248870ceaf3af7ca73f7d19aacbac4d8b192d
Jasper <jasperbower@protonmail.com>
2022-10-28 15:38:30 -0700

    fix lnswap appstore listing

--- lnswap/exports.sh
+++ lnswap/exports.sh
@@ -1,11 +1,12 @@
 export APP_LNSWAP_API_IP="10.21.21.88"
 export APP_LNSWAP_API_PORT="9008"
-export APP_LNSWAP_DASHBOARD_IP="10.21.21.89"
-export APP_LNSWAP_DASHBOARD_PORT="9009"
+export APP_LNSWAP_APP_IP="10.21.21.89"
+export APP_LNSWAP_APP_PORT="9010"
+
 
 #!/usr/bin/env bash
 
-CONFIG="${UMBREL_ROOT}/app-data/lnswap/data/boltz.conf"
+CONFIG="${UMBREL_ROOT}/app-data/stacks-lnswap/data/boltz.conf"
 
 CERT="${UMBREL_ROOT}/app-data/lightning/data/lnd/tls.cert"
 MACAROON="${UMBREL_ROOT}app-data/lightning/data/lnd/data/chain/bitcoin/${APP_BITCOIN_NETWORK}/admin.macaroon"
@@ -13,4 +14,4 @@ MACAROON="${UMBREL_ROOT}app-data/lightning/data/lnd/data/chain/bitcoin/${APP_BIT
 set -e
 
      sed -i  -e "s@CERT-PATH@$CERT@" \
-             -e "s@MACROON-PATH@$MACAROON@" "$CONFIG"
+             -e "s@MACAROON-PATH@$MACAROON@" "$CONFIG"
