d1b592b652cdc5562029a7c620170e68e7a15b5b
Jasper <jasperbower@protonmail.com>
2022-10-27 18:53:25 -0700

    move prestart to export.sh

--- lnswap/exports.sh
+++ lnswap/exports.sh
@@ -2,3 +2,15 @@ export APP_LNSWAP_API_IP="10.21.21.88"
 export APP_LNSWAP_API_PORT="9008"
 export APP_LNSWAP_DASHBOARD_IP="10.21.21.89"
 export APP_LNSWAP_DASHBOARD_PORT="9009"
+
+#!/usr/bin/env bash
+
+CONFIG="${UMBREL_ROOT}/app-data/lnswap/data/boltz.conf"
+
+CERT="${UMBREL_ROOT}/app-data/lightning/data/lnd/tls.cert"
+MACAROON="${UMBREL_ROOT}app-data/lightning/data/lnd/data/chain/bitcoin/${APP_BITCOIN_NETWORK}/admin.macaroon"
+
+set -e
+
+     sed -i  -e "s@CERT-PATH@$CERT@" \
+             -e "s@MACROON-PATH@$MACAROON@" "$CONFIG"
