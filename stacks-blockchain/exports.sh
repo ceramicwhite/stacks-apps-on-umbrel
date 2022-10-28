3d25b3632106876e0310ba2a61eda8803a8099d6
Jasper <jasperbower@protonmail.com>
2022-10-27 18:55:07 -0700

    remove prehook

--- stacks-blockchain/exports.sh
+++ stacks-blockchain/exports.sh
@@ -8,3 +8,19 @@ export APP_STACKS_API_IP="10.21.21.98"
 export APP_STACKS_API_PORT="3999"
 export APP_STACKS_DB_IP="10.21.21.97"
 export APP_STACKS_DB_PORT="5432"
+
+#!/usr/bin/env bash
+
+CONFIG="${UMBREL_ROOT}/app-data/stacks-blockchain/data/config/Config.toml"
+
+set -e
+
+     sed -i  -e "s@APP_BITCOIN_NODE_IP@$APP_BITCOIN_NODE_IP@" \
+             -e "s@APP_BITCOIN_RPC_USER@$APP_BITCOIN_RPC_USER@" \
+             -e "s@APP_BITCOIN_RPC_PASS@$APP_BITCOIN_RPC_PASS@" \
+             -e "s@APP_BITCOIN_RPC_PORT@$APP_BITCOIN_RPC_PORT@" \
+             -e "s@APP_STACKS_CORE_RPC_PORT@$APP_STACKS_CORE_RPC_PORT@" \
+             -e "s@APP_STACKS_CORE_P2P_PORT@$APP_STACKS_CORE_P2P_PORT@" \
+             -e "s@APP_STACKS_API_IP@$APP_STACKS_API_IP@" \
+             -e "s@APP_STACKS_CORE_EVENT_PORT@$APP_STACKS_CORE_EVENT_PORT@" \
+             -e "s@APP_BITCOIN_P2P_PORT@$APP_BITCOIN_P2P_PORT@" "$CONFIG"
